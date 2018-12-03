8f77990ae21ec53b3fc0b6c9f412583bbb3d2416
Rod Johnson <johnsonroda@gmail.com>
2018-12-04 10:20:39 +1100

    Initial commit from Atomist

---
+++ src/main/java/com/eric/Ericm1Application.java
@@ -0,0 +1,12 @@
+package com.eric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ericm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ericm1Application.class, args);
+	}
+}
